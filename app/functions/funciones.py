10a375d36c51c336e2c8f219cb54cd495f810ffe
Tomás del barco <tomasdelbarco07@gmail.com>
2023-11-24 17:42:27 -0300

    endpoints 3 , 4 y 5 deployables

--- app/functions/funciones.py
+++ app/functions/funciones.py
@@ -4,29 +4,33 @@ import pandas as pd
 
 
 
+df_recommend = pd.read_parquet("df_recommend.parquet")
 
+df_sentimientos = pd.read_parquet("df_sentimientos.parquet")
 
 
 
 
 #Devuelve el top 3 de juegos MÁS recomendados por usuarios para el año dado. (reviews.recommend = True y comentarios positivos/neutrales)
     #return {} #Ejemplo de retorno: [{"Puesto 1" : X}, {"Puesto 2" : Y},{"Puesto 3" : Z}]
-def usersbestdeveloper(anio,df):
-    df_f = df[df['posted'].str.contains(str(anio))] #filtro el df al año
+def juegos_mas_recomendados(anio):
+    df_f = df_recommend[df_recommend['posted'].str.contains(str(anio))] #filtro el df al año
     df_f = df_f[(df_f['sentimiento'] == 2) | (df_f['recommend'] == True)]#filtro el df a los que al menos poseen uno de las 2 cosas que vamos a contar
 
     conteo_sentimiento = df_f[df_f['sentimiento'] == 2].groupby('item_id')['sentimiento'].count()
-    conteo_recommend = df_f[df_f['recommend'] == False].groupby('item_id')['recommend'].count()
+    conteo_recommend = df_f[df_f['recommend'] == True].groupby('item_id')['recommend'].count()
 
-    conteo_df = pd.df( conteo_recommend,conteo_sentimiento, on='item_id', how='inner')
+    conteo_df = pd.merge( conteo_recommend,conteo_sentimiento, on='item_id', how='inner')
 
-    df_f = df_f.df(conteo_df,how="inner",left_on="id",right_on="item_id")
+    df_f = df_f.merge(conteo_df,how="inner",left_on="id",right_on="item_id")
 
     df_f = df_f.drop_duplicates(subset=['item_id'])
 
     df_f = df_f.sort_values(by=['recommend_y','sentimiento_y'], ascending=[False, False])
 
-    return df_f
+    result_list = [{"Puesto " + str(i + 1): item_id} for i, item_id in enumerate(df_f['item_id'][:3])]
+
+    return result_list
 
 
 
@@ -34,46 +38,41 @@ def usersbestdeveloper(anio,df):
 
 #Devuelve el top 3 de desarrolladoras con juegos MENOS recomendados por usuarios para el año dado. (reviews.recommend = False y comentarios negativos)
     #return {} #Ejemplo de retorno: [{"Puesto 1" : X}, {"Puesto 2" : Y},{"Puesto 3" : Z}]
-def usersworstdeveloper(anio,df):
-    df_f = df[df['posted'].str.contains(str(anio))] #filtro el df al año
+def developer_menos_recomendadas(anio):
+    df_f = df_recommend[df_recommend['posted'].str.contains(str(anio))] #filtro el df al año
     df_f = df_f[(df_f['sentimiento'] == 0) | (df_f['recommend'] == False)]#filtro el df a los que al menos poseen uno de las 2 cosas que vamos a contar
 
-    conteo_sentimiento = df_f[df_f['sentimiento'] == 2].groupby('item_id')['sentimiento'].count()
-    conteo_recommend = df_f[df_f['recommend'] == False].groupby('item_id')['recommend'].count()
+    conteo_sentimiento = df_f[df_f['sentimiento'] == 0].groupby('developer')['sentimiento'].count()
+    conteo_recommend = df_f[df_f['recommend'] == False].groupby('developer')['recommend'].count()
 
-    conteo_df = pd.df( conteo_recommend,conteo_sentimiento, on='item_id', how='inner')
+    conteo_df = pd.merge( conteo_recommend,conteo_sentimiento, on='developer', how='inner')
 
-    df_f = df_f.df(conteo_df,how="inner",left_on="id",right_on="item_id")
+    df_f = df_f.merge(conteo_df,how="inner",on="developer")
 
-    df_f = df_f.drop_duplicates(subset=['item_id'])
+    df_f = df_f.drop_duplicates(subset=['developer'])
 
     df_f = df_f.sort_values(by=['recommend_y','sentimiento_y'], ascending=[False, False])
 
-    return df_f
+    result_list = [{"Puesto " + str(i + 1): developer} for i, developer in enumerate(df_f['developer'][:3])]
+
+    return result_list
 
 
 
 
 #Según la empresa desarrolladora, se devuelve un diccionario con el nombre de la desarrolladora como llave y una lista con la cantidad total de registros de reseñas de usuarios que se encuentren categorizados con un análisis de sentimiento como valor.
    # return {} #Ejemplo de retorno: {'Valve' : [Negative = 182, Neutral = 120, Positive = 278]}
-def contar_sentimientos_por_desarrollador(developer,df):
+def contar_sentimientos_por_desarrollador(developer):
     # Filtra el DataFrame para obtener solo las filas correspondientes al desarrollador dado
     #carga el df sentiment analisis
 
-    df_temporal = df[df['developer'] == developer]#filtra el df donde las columnas correspondan al input
+    df_temporal = df_sentimientos[df_sentimientos['developer'] == developer]#filtra el df donde las columnas correspondan al input
 
     # Cuenta la cantidad de sentimientos 0, 1 y 2
     count_sentimiento_0 = df_temporal[df_temporal['sentimiento'] == 0].count()
     count_sentimiento_1 = df_temporal[df_temporal['sentimiento'] == 1].count()
     count_sentimiento_2 = df_temporal[df_temporal['sentimiento'] == 2].count()
 
-    # Devuelve los resultados
-    return {
-        'sentimiento_0': count_sentimiento_0.values[0],
-        'sentimiento_1': count_sentimiento_1.values[0],
-        'sentimiento_2': count_sentimiento_2.values[0]
-    }
 
-# Ejemplo de uso
-developer_stats = contar_sentimientos_por_desarrollador('Tripwire Interactive')
-print(developer_stats)
+    # Devuelve los resultados
+    return str({'Negative': count_sentimiento_0.values[0],'Neutral': count_sentimiento_1.values[0],'Positive': count_sentimiento_2.values[0]})
--- app/main.py
+++ app/main.py
@@ -1,17 +1,11 @@
 from fastapi import FastAPI
 import numpy as np
 import pandas as pd
+import functions.funciones as f
 
 app = FastAPI()
 
 
-df_sg = pd.read_parquet("steam_games.parquet")
-
-df_ud = pd.read_parquet("user_data.parquet")
-df_ur = pd.read_parquet("reviews_dict.parquet")
-
-df_ui = pd.read_parquet("users_items.parquet")
-df_dict_ui = pd.read_parquet("uitems.parquet")
 
 
 
@@ -21,57 +15,40 @@ df_dict_ui = pd.read_parquet("uitems.parquet")
 async def root():
     return "¡Hola FastAPI!"
 
-print("todo se ejecuto")
-
-#./venv/Scripts/Activate
-#deactivate
-
-#pip install "fastapi[all]" esto instala uvicorn tambien
-
-#o puedes instalarlo por partes
-#pip install fastapi
-
-#pip install "uvicorn[standard]"
-
-#pip freeze > Requeriment.txt crea el archivo requeriments.txt~(dentro de el venv)
-
-#python main.py (hay que correrlo desde dentro de el venv)
-#uvicorn api:app --reload
-
-
-#pip install -r "requirements.txt"
-
 #-------------------------------------------------------------------------------------------------------------------------------------
 
-
-@app.get("/PlayTimeGenre")
-async def PlayTimeGenre( genero : str ): #Debe devolver año con mas horas jugadas para dicho género.
-    
-    return {}#Ejemplo de retorno: {"Año de lanzamiento con más horas jugadas para Género X" : 2013}
-
-@app.get("/UserForGenre")
-async def UserForGenre( genero : str ): #Debe devolver el usuario que acumula más horas jugadas para el género dado y una lista de la acumulación de horas jugadas por año.
-    return {} #Ejemplo de retorno: {"Usuario con más horas jugadas para Género X" : us213ndjss09sdf, "Horas jugadas":[{Año: 2013, Horas: 203}, {Año: 2012, Horas: 100}, {Año: 2011, Horas: 23}]}
-
-
-
-
+#
+#@app.get("/PlayTimeGenre")
+#async def PlayTimeGenre( genero : str ): #Debe devolver año con mas horas jugadas para dicho género.
+#    
+#    return {}#Ejemplo de retorno: {"Año de lanzamiento con más horas jugadas para Género X" : 2013}
+#
+#@app.get("/UserForGenre")
+#async def UserForGenre( genero : str ): #Debe devolver el usuario que acumula más horas jugadas para el género dado y una lista de la acumulación de horas jugadas por año.
+#    return {} #Ejemplo de retorno: {"Usuario con más horas jugadas para Género X" : us213ndjss09sdf, "Horas jugadas":[{Año: 2013, Horas: 203}, {Año: 2012, Horas: 100}, {Año: 2011, Horas: 23}]}
+#
 
 
 
 @app.get("/UsersRecommend")
-async def UsersRecommend( año : int ): #Devuelve el top 3 de juegos MÁS recomendados por usuarios para el año dado. (reviews.recommend = True y comentarios positivos/neutrales)
-    return {} #Ejemplo de retorno: [{"Puesto 1" : X}, {"Puesto 2" : Y},{"Puesto 3" : Z}]
+async def UsersRecommend( año : int ):
+    #Devuelve el top 3 de juegos MÁS recomendados por usuarios para el año dado. (reviews.recommend = True y comentarios positivos/neutrales)
+    return f.juegos_mas_recomendados(año) #Ejemplo de retorno: [{"Puesto 1" : X}, {"Puesto 2" : Y},{"Puesto 3" : Z}]
+
+
+
 
 @app.get("/UsersWorstDeveloper")
 async def UsersWorstDeveloper( año : int ): #Devuelve el top 3 de desarrolladoras con juegos MENOS recomendados por usuarios para el año dado. (reviews.recommend = False y comentarios negativos)
-    return {} #Ejemplo de retorno: [{"Puesto 1" : X}, {"Puesto 2" : Y},{"Puesto 3" : Z}]
+    return f.developer_menos_recomendadas(año) #Ejemplo de retorno: [{"Puesto 1" : X}, {"Puesto 2" : Y},{"Puesto 3" : Z}]
 
 
 
 @app.get("/sentiment_analysis")
 async def sentiment_analysis( empresa_desarrolladora : str ): #Según la empresa desarrolladora, se devuelve un diccionario con el nombre de la desarrolladora como llave y una lista con la cantidad total de registros de reseñas de usuarios que se encuentren categorizados con un análisis de sentimiento como valor.
-    return {} #Ejemplo de retorno: {'Valve' : [Negative = 182, Neutral = 120, Positive = 278]}
+    return f.contar_sentimientos_por_desarrollador(empresa_desarrolladora)
+
+ #Ejemplo de retorno: {'Valve' : [Negative = 182, Neutral = 120, Positive = 278]}
 
 #Modelo de aprendizaje automático:
 #
@@ -87,11 +64,11 @@ async def sentiment_analysis( empresa_desarrolladora : str ): #Según la empresa
 
 #Si es un sistema de recomendación item-item:
 
-@app.get("/recomendacion_juego")
-async def recomendacion_juego( id_producto : int ): #Ingresando el id de producto, deberíamos recibir una lista con 5 juegos recomendados similares al ingresado.
-    return {}
-
-#Si es un sistema de recomendación user-item:
-@app.get("/recommendacion_usuario")
-async def recomendacion_usuario( id_de_usuario : int ): #Ingresando el id de un usuario, deberíamos recibir una lista con 5 juegos recomendados para dicho usuario.
-    return {}
+#@app.get("/recomendacion_juego")
+#async def recomendacion_juego( id_producto : int ): #Ingresando el id de producto, deberíamos recibir una lista con 5 juegos recomendados similares al ingresado.
+#    return {}
+#
+##Si es un sistema de recomendación user-item:
+#@app.get("/recommendacion_usuario")
+#async def recomendacion_usuario( id_de_usuario : int ): #Ingresando el id de un usuario, deberíamos recibir una lista con 5 juegos recomendados para dicho usuario.
+#    return {}
