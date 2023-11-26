40703cc1f4ae4a1e1c43ab6585db88edd0985142
Tomás del barco <tomasdelbarco07@gmail.com>
2023-11-26 12:37:38 -0300

    docstring

--- app/main.py
+++ app/main.py
@@ -1,39 +1,85 @@
 from fastapi import FastAPI
+import functions.modelo as ml
 import numpy as np
 import pandas as pd
 import functions.funciones as f
+from enum import Enum
+
 
 app = FastAPI()
-
-
-
-
-
 
 
 @app.get("/")
 async def root():
     return "¡Hola FastAPI!"
 
+
+
+#class GeneroEnum(str, Enum):
+#    alexnet = "alexnet"
+#    resnet = "resnet"
+#    lenet = "lenet"
+
+
+class ValorNumericoEnum(int, Enum):#lista de algunos de los valores posibles de el campo de recomendacion de juegos
+    motogp_13_champions = 240612
+    euro_truck_simulator_raven_pack = 318521
+    warhammer_space_wolf_great_awakening = 760720
+    hunter_x_hunter = 545630
+    final_fantasy_vi = 382900
+    berserk_golden_age_arc_ii = 464420
+    gotham_city_impostors_beaky = 216450
+    inuyasha_movie_affections = 488370
+    euro_truck_simulator_going_east = 227310
+    lego_pirates_caribbean = 311770
+    payday_shadow_raid_heist = 267382
+    star_wars_tie_fighter = 355250
+    tales_of_zestiria_costumes_set = 382464
+    driver_pro_2017 = 710540
+    rpg_maker_casino_tile_pack = 283541
+    castlevania_mirror_of_fate_hd = 282530
+    lego_marvel_super_heroes_2_guardians = 720690
+    battle_of_empires_blazing_guns = 340620
+    total_war_rome_ii_culture_pack = 273381
+    call_of_duty_rezurrection_mac = 214649
+    wwe_2k18_myplayer_kick_start = 706080
+    yugioh_duel_links = 601510
+
+
+
+
+
 #-------------------------------------------------------------------------------------------------------------------------------------
 
 
-
-
- #Debe devolver año con mas horas jugadas para dicho género.
-@app.get("/PlayTimeGenre")
+@app.get("/PlayTimeGenre/{genero}")
 async def PlayTimeGenre( genero : str ):
+    """
+    recibe un genero
+
+    Devuelve el año con más horas jugadas para el género dado.
+
+    Ejemplo de retorno:
+    {"Año de lanzamiento con más horas jugadas para Género X": 2013}
+    """
     return f.horas_año_genero(genero)
-#Ejemplo de retorno: {"Año de lanzamiento con más horas jugadas para Género X" : 2013}
 
 
 
 
-#Debe devolver el usuario que acumula más horas jugadas para el género dado y una lista de la acumulación de horas jugadas por año.
-@app.get("/UserForGenre")
+
+@app.get("/UserForGenre/")
 async def UserForGenre( genero : str ): 
+    """
+    recibe un genero
+
+    Devuelve el usuario que acumula más horas jugadas para el género dado y una lista de la acumulación de horas jugadas por año.
+
+    Ejemplo de retorno:
+    {"Usuario con más horas jugadas para Género X": "us213ndjss09sdf", "Horas jugadas": [{"Año": 2013, "Horas": 203}, {"Año": 2012, "Horas": 100}, {"Año": 2011, "Horas": 23}]}
+    """
     return f.usuario_genero(genero) 
-#Ejemplo de retorno: {"Usuario con más horas jugadas para Género X" : us213ndjss09sdf, "Horas jugadas":[{Año: 2013, Horas: 203}, {Año: 2012, Horas: 100}, {Año: 2011, Horas: 23}]}
+
 
 
 
@@ -41,17 +87,29 @@ async def UserForGenre( genero : str ):
 #Devuelve el top 3 de juegos MÁS recomendados por usuarios para el año dado. (reviews.recommend = True y comentarios positivos/neutrales)
 @app.get("/UsersRecommend")
 async def UsersRecommend( año : int ):
+    """
+    Devuelve el top 3 de juegos MÁS recomendados por usuarios para el año dado.
+
+    Ejemplo de retorno:
+    [{"Puesto 1": X}, {"Puesto 2": Y}, {"Puesto 3": Z}]
+    """
     return f.juegos_mas_recomendados(año)
-#Ejemplo de retorno: [{"Puesto 1" : X}, {"Puesto 2" : Y},{"Puesto 3" : Z}]
+
 
 
 
 
 #Devuelve el top 3 de desarrolladoras con juegos MENOS recomendados por usuarios para el año dado. (reviews.recommend = False y comentarios negativos)
 @app.get("/UsersWorstDeveloper")
-async def UsersWorstDeveloper( año : int ): 
+async def UsersWorstDeveloper( año : int ):
+    """
+    Devuelve el top 3 de desarrolladoras con juegos MENOS recomendados por usuarios para el año dado.
+
+    Ejemplo de retorno:
+    [{"Puesto 1": X}, {"Puesto 2": Y}, {"Puesto 3": Z}]
+    """
     return f.developer_menos_recomendadas(año)
-#Ejemplo de retorno: [{"Puesto 1" : X}, {"Puesto 2" : Y},{"Puesto 3" : Z}]
+
 
 
 
@@ -59,8 +117,13 @@ async def UsersWorstDeveloper( año : int ):
 #Según la empresa desarrolladora, se devuelve un diccionario con el nombre de la desarrolladora como llave y una lista con la cantidad total de registros de reseñas de usuarios que se encuentren categorizados con un análisis de sentimiento como valor.
 @app.get("/sentiment_analysis")
 async def sentiment_analysis( empresa_desarrolladora : str ): 
+    """
+    Según la empresa desarrolladora, se devuelve un diccionario con el nombre de la desarrolladora como llave y una lista con la cantidad total de registros de reseñas de usuarios que se encuentren categorizados con un análisis de sentimiento como valor.
+
+    Ejemplo de retorno:
+    {'Valve': {'Negative': 182, 'Neutral': 120, 'Positive': 278}}
+    """
     return f.contar_sentimientos_por_desarrollador(empresa_desarrolladora)
- #Ejemplo de retorno: {'Valve' : [Negative = 182, Neutral = 120, Positive = 278]}
 
 
 
@@ -80,13 +143,25 @@ async def sentiment_analysis( empresa_desarrolladora : str ):
 
 #Si es un sistema de recomendación item-item:
 
-#@app.get("/recomendacion_juego")
-#async def recomendacion_juego( id_producto : int ): #Ingresando el id de producto, deberíamos recibir una lista con 5 juegos recomendados similares al ingresado.
-#    return {}
-#
+
+
+#Ingresando el id de producto, deberíamos recibir una lista con 5 juegos recomendados similares al ingresado.
+@app.get("/recomendacion_juego")
+async def recomendacion_juego( id_producto : ValorNumericoEnum ): 
+    """
+    Devuelve una lista con 5 juegos recomendados similares al ingresado.
+
+    Args:
+        id_producto (int): El ID del producto para el cual se desean obtener recomendaciones.
+
+    Returns:
+        List[int]: Una lista con los IDs de los juegos recomendados.
+    """
+    return ml.obtener_recomendaciones(id_producto)
+
+
+
 ##Si es un sistema de recomendación user-item:
 #@app.get("/recommendacion_usuario")
 #async def recomendacion_usuario( id_de_usuario : int ): #Ingresando el id de un usuario, deberíamos recibir una lista con 5 juegos recomendados para dicho usuario.
 #    return {}
-
-#uvicorn main:app --reload
