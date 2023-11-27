eb9e6096c095a3bc919d31c3445764aeba47b486
Tomás del barco <tomasdelbarco07@gmail.com>
2023-11-27 10:38:42 -0300

    cambiando retund de funcion  userforgenre

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from starlette.responses import RedirectResponse
 import functions.modelo as ml
-import numpy as np
-import pandas as pd
+#import numpy as np
+#import pandas as pd
 import functions.funciones as f
 from enum import Enum
 
